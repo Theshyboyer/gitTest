f936f4207074f931abc4e3f264fab1d9915f9f23
wt_glb <wt_glb@wt.com>
2021-08-04 15:44:20 +0800

    third commit

--- src/main/java/com/wt/git/gitTest.java
+++ src/main/java/com/wt/git/gitTest.java
@@ -10,5 +10,6 @@ public class gitTest {
     public static void main(String[] args) {
         System.out.println("git test");
         System.out.println("git test update");
+        System.out.println("git test update2");
     }
 }
