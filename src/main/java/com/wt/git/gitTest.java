5c7cf72f72a0da53f58a9e7b1ce2bd9571624fc5
wt_glb <wt_glb@wt.com>
2021-08-04 15:38:11 +0800

    first commit

---
+++ src/main/java/com/wt/git/gitTest.java
@@ -0,0 +1,13 @@
+package com.wt.git;
+
+/**
+ * @program: gitTest
+ * @description:
+ * @author: Mr.Wang
+ * @create: 2021-08-04 15:28
+ **/
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("git test");
+    }
+}
